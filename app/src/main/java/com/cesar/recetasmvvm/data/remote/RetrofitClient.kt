9edbf36cbc447c5e12ee8ab92d43bd3f7fea1281
cvargas2003 <cesarvargas1724@gmail.com>
2026-05-23 15:11:52 -0500

    MVVM CON BACKEND FUNCIONAL

--- app/src/main/java/com/cesar/recetasmvvm/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/cesar/recetasmvvm/data/remote/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "http://192.168.40.36:3000/"
 
     val api: RecipeApiService by lazy {
 
